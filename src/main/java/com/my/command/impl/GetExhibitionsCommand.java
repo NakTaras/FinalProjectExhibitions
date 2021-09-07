0e586d2dda5439a244ef90733c317ed771c487b1
NakTaras <51298366+NakTaras@users.noreply.github.com>
2021-09-07 12:44:10 +0300

    Test commit

--- src/main/java/com/my/command/impl/GetExhibitionsCommand.java
+++ src/main/java/com/my/command/impl/GetExhibitionsCommand.java
@@ -1,6 +1,8 @@
 package com.my.command.impl;
 
 import com.my.command.Command;
+import com.my.db.dao.ExhibitionDao;
+import com.my.db.dao.impl.ExhibitionDaoImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 public class GetExhibitionsCommand implements Command {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        ExhibitionDao exhibitionDao = ExhibitionDaoImpl.getInstance();
+        exhibitionDao.getAllExhibitions();
         return null;
     }
 }
